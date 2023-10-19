1b87cc7b50a7e65197600a9c397ae36fcb6c6630
giu-g <133990052+giu-g@users.noreply.github.com>
2023-10-19 09:28:33 -0300

    Add files via upload

---
+++ PrjGame/src/main/java/com/senai/giuliag/PrjGame/services/JogoService.java
@@ -0,0 +1,36 @@
+package com.senai.giuliag.PrjGame.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.senai.giuliag.PrjGame.entities.Jogo;
+import com.senai.giuliag.PrjGame.repositories.JogoRepository;
+
+@Service
+public class JogoService {
+	private final JogoRepository jogoRepository;
+	
+	@Autowired
+	public JogoService(JogoRepository jogoRepository) {
+		this.jogoRepository = jogoRepository;
+	}
+	
+	public Jogo saveJogo(Jogo jogo) {
+		return jogoRepository.save(jogo);
+	}
+	
+	public Jogo getJogoById(Long id) {
+		return jogoRepository.findById(id).orElse(null);
+	}
+
+	public List<Jogo> getAllJogos() {
+		return jogoRepository.findAll();
+	}
+
+	public void deleteJogo(Long id) {
+		jogoRepository.deleteById(id);
+	}
+
+}
